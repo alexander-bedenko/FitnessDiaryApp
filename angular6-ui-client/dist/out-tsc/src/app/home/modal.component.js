dcfc31c0c3a87c035e567562be7f9307fd8ffae4
Alexander Bedenko <alexander.bedenko@sorted.com>
2019-05-17 11:55:13 +0300

    Added modal popup

---
+++ angular6-ui-client/dist/out-tsc/src/app/home/modal.component.js
@@ -0,0 +1,38 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var ng_bootstrap_1 = require("@ng-bootstrap/ng-bootstrap");
+var NgbdModalComponent = /** @class */ (function () {
+    function NgbdModalComponent(activeModal) {
+        this.activeModal = activeModal;
+    }
+    NgbdModalComponent_1 = NgbdModalComponent;
+    NgbdModalComponent.prototype.open = function () {
+        var modalRef = this.modalService.open(NgbdModalComponent_1);
+        modalRef.componentInstance.content = this.content;
+    };
+    __decorate([
+        core_1.Input(),
+        __metadata("design:type", Object)
+    ], NgbdModalComponent.prototype, "content", void 0);
+    NgbdModalComponent = NgbdModalComponent_1 = __decorate([
+        core_1.Component({
+            selector: 'ngbd-modal-component',
+            templateUrl: './modal.component.html'
+        }),
+        __metadata("design:paramtypes", [ng_bootstrap_1.NgbModal])
+    ], NgbdModalComponent);
+    return NgbdModalComponent;
+    var NgbdModalComponent_1;
+}());
+exports.NgbdModalComponent = NgbdModalComponent;
+//# sourceMappingURL=modal.component.js.map
---
+++ angular6-ui-client/src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models';
+import { UserService } from '../_services';
+import { NgbModal, NgbActiveModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+@Component(
+    {
+        templateUrl: 'home.component.html'
+    })
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+    closeResult: string;
+
+    constructor(private userService: UserService, private modalService: NgbModal, public activeModal: NgbActiveModal) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).pipe(first()).subscribe(() => { 
+            this.loadAllUsers() 
+        });
+    }
+
+    open(content) {
+        this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+          this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+      }
+      private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+          return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+          return 'by clicking on a backdrop';
+        } else {
+          return  `with: ${reason}`;
+        }
+      }
+    private loadAllUsers() {
+        this.userService.getAll().pipe(first()).subscribe(users => { 
+            this.users = users; 
+        });
+    }
+}
